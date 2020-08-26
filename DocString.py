a2e946109964889cb18babafc224d7e6e95386d5
pboopathi <pboopathi@ebay.com>
2020-08-26 14:08:04 -0700

    Keyowrd Arguments excercise practiced

---
+++ DocString.py
@@ -0,0 +1,14 @@
+import pickle
+def square(n):
+    '''Takes in a number n, returns square of n'''
+
+    return n*2
+
+#__doc__ attribute prints the documentation
+print(square.__doc__)
+
+# prints the pickle module documentation
+print(pickle.__doc__)
+
+if __name__ == "__main__":
+    pass
---
+++ KeywordArgument.py
@@ -0,0 +1,25 @@
+# kwarg = value
+
+def parrot(voltage, state='Priya'):
+    print(voltage, end=' ')
+
+    print(parrot(1000))
+
+    # unpack argument list
+
+
+def UnpackingArgumentList(*args):
+    output = range(*args)
+    print(output)
+    pass
+
+# * unpacks the list and tuple
+listInput = [3, 6,9]
+UnpackingArgumentList(*listInput)
+
+
+
+
+# not sure why this would fail
+if __name__ == "__main__":
+    pass
