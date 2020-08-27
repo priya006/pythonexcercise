42e7e15b2d3eb1e62958722a1411b992178b9769
pboopathi <pboopathi@ebay.com>
2020-08-27 15:37:49 -0700

    Extracted to a method

--- SimplePrograms/MoveFiles.py
+++ SimplePrograms/MoveFiles.py
@@ -1,14 +1,19 @@
 import shutil
 import os
 
-sourcepath = '/Users/pboopathi/Downloads/Source'
-destinationpath = '/Users/pboopathi/Downloads/Destination'
 
-files = os.listdir(sourcepath)
-print("The files to be moved ",files)
-print("Source path", sourcepath)
-print("Destination Path", destinationpath)
-for f in files:
-    source = sourcepath+"/"+f
-    shutil.move(source, destinationpath)
+def move_file(sourcepath, destinationpath):
+    files = os.listdir(sourcepath)
+    print("The files to be moved ", files)
+    print("Source path", sourcepath)
+    print("Destination Path", destinationpath)
+    for f in files:
+        source = sourcepath + "/" + f
+        shutil.move(source, destinationpath)
 
+
+if __name__ == "__main__":
+    sourcepath = '/Users/pboopathi/Downloads/Source'
+    destinationpath = '/Users/pboopathi/Downloads/Destination'
+    move_file(sourcepath, destinationpath)
+    pass
