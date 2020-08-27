1db01552ccecb6da97425eb2156f2cf615c62c6d
pboopathi <pboopathi@ebay.com>
2020-08-27 16:26:23 -0700

    Copied the doc files from Downloads folder and created a new folder and copied them over

--- SimplePrograms/MoveFiles.py
+++ SimplePrograms/MoveFiles.py
@@ -1,8 +1,12 @@
+import glob
 import shutil
 import os
 
+path = '/Users/pboopathi/Downloads/'
 
 def move_file(sourcepath, destinationpath):
+    ''' Move files from one folder  to another folder '''
+
     files = os.listdir(sourcepath)
     print("The files to be moved ", files)
     print("Source path", sourcepath)
@@ -12,8 +16,24 @@ def move_file(sourcepath, destinationpath):
         shutil.move(source, destinationpath)
 
 
+def move_docfiles():
+    dst_fldr = path + 'DocFiles'
+    try:
+        os.makedirs(path + 'DocFiles')  # it creates the destination folder 'DocFiles'
+    except:
+        print('Folder already exist or some error')
+
+    files = glob.iglob(os.path.join(path, "*.doc"))
+    for doc_files in files:
+        if os.path.isfile(doc_files):
+            print('Doc File is moved', doc_files)
+            shutil.move(doc_files, dst_fldr)
+            pass
+
+
 if __name__ == "__main__":
-    sourcepath = '/Users/pboopathi/Downloads/Source'
-    destinationpath = '/Users/pboopathi/Downloads/Destination'
-    move_file(sourcepath, destinationpath)
+    sourcefolder = 'Source'
+    Destinationfolder = 'Destination'
+    move_file(path + sourcefolder, path + Destinationfolder)
+    move_docfiles()
     pass
