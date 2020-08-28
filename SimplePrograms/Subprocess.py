bd693b5f145af16709abe679002d5d0cecb235b3
pboopathi <pboopathi@ebay.com>
2020-08-28 11:14:15 -0700

    Using subprocess popen writting to file the output of the shell command

--- SimplePrograms/Subprocess.py
+++ SimplePrograms/Subprocess.py
@@ -7,7 +7,28 @@ print(f'Check File System Disk Space Usage')
 print('*********************************')
 subprocess.call(['df', '-h'])
 
+print('********************************************************')
+print(f'Check Information of all File System Disk Space Usage')
+print('********************************************************')
+subprocess.call(['df', '-a'])
+
+print('************')
+print(f'List Files')
+print('************')
+subprocess.call(['ls', '-lah'])
+
+
+def popen_redirectoutputtofile():
+    path_to_output_file = "/Users/pboopathi/Desktop/python"
+    output = open(path_to_output_file, 'w+')
+    print('**************************************************')
+    print('Writting to file in the path --> ', path_to_output_file)
+    process = subprocess.Popen(["ls", "-a"], stdout=output, stderr=subprocess.PIPE, universal_newlines=True)
+    output, errors = process.communicate()
+
+    pass
 
 
 if __name__ == "__main__":
+    popen_redirectoutputtofile()
     pass
